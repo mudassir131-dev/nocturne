d811fe76741af137e16b540376f98261f367c67d
mudassir131-dev <touseefpara7@gmail.com>
2026-06-27 21:55:44 +0530

    chore: bump version to 2.2.3.4.02 for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 23
-        versionName = "2.2.3.4.01"
+        versionName = "2.2.3.4.02"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
