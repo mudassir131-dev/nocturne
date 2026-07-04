4780cf26658656a5b73005e7cafd3403a8b33a84
mudassir131-dev <touseefpara7@gmail.com>
2026-07-04 11:53:45 +0530

    chore: bump version to 2.2.3.4.08 (29)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.mudassir131.nocturne"
         minSdk = 26
         targetSdk = 36
-        versionCode = 28
-        versionName = "2.2.3.4.07"
+        versionCode = 29
+        versionName = "2.2.3.4.08"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
